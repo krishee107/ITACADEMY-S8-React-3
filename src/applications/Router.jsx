7481d9a1625cb90d1ba6790fa34a9932ec35d5fb
Cristina Ferrer <krishee107@gmail.com>
2023-04-18 13:55:56 +0200

    Añadido login/registro al nav y al router

--- src/applications/Router.jsx
+++ src/applications/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Starships from '../pages/Starships/Starships';
 import Ship from '../components/Ship/Ship';
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
 
 const Router = () => (
     <BrowserRouter>
@@ -9,6 +11,8 @@ const Router = () => (
             <Route index element={<Home />} />
             <Route path="/starships/" element={<Starships />} />
             <Route path="/starships/:id" element={<Ship />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Register />} />
             <Route path="*" element={<div>404</div>} />
         </Routes>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,16 @@ import Logo from '../../assets/logo.jpg'
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
-        <img src={Logo} alt="Logo" />
+      <div className="header">
+        <div> </div>
+        <div className="logo">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className="userOptions">
+          <Link to="/login">LOG IN</Link>
+          <span>//</span>
+          <Link to="/registro">SIGN UP</Link>
+        </div>
       </div>
       <ul>
         <li><Link to="/">Home</Link></li>
